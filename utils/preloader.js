810c0a011703636c94f6e9d14483881da6ce0135
Yehor Korablin <yegor_ka2004@ukr.net>
2022-12-27 16:36:22 +0200

    Fast savings...

---
+++ utils/preloader.js
@@ -0,0 +1,8 @@
+let loader = document.querySelectorAll(".preloader")[0];
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    loader.style.display = "none";
+    document.body.classList.remove("hidden");
+  }, 500);
+});
